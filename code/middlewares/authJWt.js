c9604df665923d36127f663c224c0bb726e6c679
Kevin Aguirre <kevinnaguirre9@gmail.com>
2021-09-04 21:04:10 -0500

    SSe verifica el token cuando se crea o actualiza un producto

--- code/middlewares/authJWt.js
+++ code/middlewares/authJWt.js
@@ -1,25 +1,25 @@
 const config = require('../configuracion')
 const pool = require('../bd')
+const jwt = require('jsonwebtoken')
 
-const verifyToken = async(req, res, next) => {
+const verificarToken = async(req, res, next) => {
      let token = req.headers["x-access-token"];
      if (!token) return res.status(403).json({ message: "Token no proporcionado" });
 
      try {
-          const decodificado = jwt.verify(token, config.JWT_SECRET);
+          const decodificado = jwt.verify(token, config.SECRET);
           req.empleadoId = decodificado.id
 
           const empleado = await pool.query('SELECT * FROM empleado WHERE cedula = $1;', [req.empleadoId])
 
-          if (empleado.rowCount === 0) return res.status(404).json({ message: "Empleado no encontrado" });
+          if (!empleado.rowCount > 0) return res.status(404).json({ message: "Empleado no encontrado" });
 
           next();
      } catch (error) {
-          console.log(error)
-          return res.status(401).json({ message: "No autorizado!" });
+          return res.status(401).json({ message: "No autorizado! Token inválido" });
      }
 }
 
 module.exports = {
-     verifyToken
+     verificarToken
 }
